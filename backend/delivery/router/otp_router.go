32d54b118f4ec7b892f708c7a7437d2ce400b38b
c7-2know <betelhemyimamd@gmail.com>
2024-11-21 04:34:37 +0300

    update router

--- backend/delivery/router/otp_router.go
+++ backend/delivery/router/otp_router.go
@@ -1,6 +1,7 @@
 package route
 
 import (
+	"backend-starter-project/bootstrap"
 	"backend-starter-project/delivery/controller"
 	"backend-starter-project/infrastructure/twilio"
 	"backend-starter-project/service"
@@ -8,8 +9,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func NewOTPRouter(group *gin.RouterGroup) {
-	twilioService := twilio.NewTwilioService("AC70e6cd6a8ea6f7b1052e0fa8fc89c0af", "215ede45175d11974d0e4e660d4c3335", "+251937380008")
+func NewOTPRouter(group *gin.RouterGroup,env *bootstrap.Env) {
+	twilioService := twilio.NewTwilioService(env.TwilloAccountSID, env.TwilloAuthToken,env.TwilloNumber)
 	otpService := service.NewOTPService(twilioService)
 	otpController := controller.NewOtpController(otpService)
 	group.POST("/send", otpController.SendOtp)
